e7403c1d1034db8d2f6145abee74fd8b7459574a
Mukund Kedia <118162112+banditMukund@users.noreply.github.com>
2024-11-28 17:20:53 +0530

    Update CIS_Config_Script_Windows_Server_2019_MS.ps1

--- CIS_Config_Script_Windows_Server_2019_MS.ps1
+++ CIS_Config_Script_Windows_Server_2019_MS.ps1
@@ -461,8 +461,6 @@ $testcase = "9.1.2 (L1) Ensure 'Windows Firewall: Domain: Inbound connections' i
 Write-Output "$testcase"
 Get-ItemPropertyValue -Path 'HKLM:\Software\Policies\Microsoft\WindowsFirewall\DomainProfile' -Name 'DefaultInboundAction';
 Write-Output '====';
-#Write-Output "$testcase"
-#Get-ItemPropertyValue -Path 'HKLM:\Software\Policies\Microsoft\WindowsFirewall\DomainProfile' -Name 'DefaultOutboundAction';
 $testcase = "9.1.3 (L1) Ensure 'Windows Firewall: Domain: Settings: Display a notification' is set to 'No'"
 Write-Output "$testcase"
 Get-ItemPropertyValue -Path 'HKLM:\Software\Policies\Microsoft\WindowsFirewall\DomainProfile' -Name 'DisableNotifications';
@@ -687,6 +685,8 @@ Write-Output "$testcase"
 Get-ItemPropertyValue -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters' -Name 'SMB1';
 Write-Output '====';
 $testcase = "18.4.5 (L1) Ensure 'Enable Certificate Padding' is set to 'Enabled'"
+Write-Output "$testcase"
+Get-ItemPropertyValue -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System\KDC' -Name 'EnableCertPadding'
 Write-Output '====';
 $testcase = "18.4.6 (L1) Ensure 'Enable Structured Exception Handling Overwrite Protection (SEHOP)' is set to 'Enabled'"
 Write-Output "$testcase"
